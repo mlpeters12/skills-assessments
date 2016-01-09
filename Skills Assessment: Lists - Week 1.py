1bcb304da6993f9041b018cf039cc9b4e94cf876
Megan Peterson <Meganerd@Megans-MacBook-Pro.local>
2016-01-09 14:32:41 -0800

    fixed index item

--- Skills Assessment: Lists - Week 1.py
+++ Skills Assessment: Lists - Week 1.py
@@ -32,27 +32,27 @@ def all_even(number_list):
     return evens 
 
 
-# def print_indexes(my_list):
-#     """Print the index of each item in the input_list, followed by the item itself.
+def print_indexes(my_list):
+    """Print the index of each item in the input_list, followed by the item itself.
 
-#     Do this without using a counting variable---that is, don't do something
-#     like this:
+    Do this without using a counting variable---that is, don't do something
+    like this:
 
-#         count = 0
-#         for item in list:
-#             print count
-#             count = count + 1
+        count = 0
+        for item in list:
+            print count
+            count = count + 1
 
-#     Output should look like this:
+    Output should look like this:
 
-#         >>> print_indexes(["Toyota", "Jeep", "Volvo"])
-#         0 Toyota
-#         1 Jeep
-#         2 Volvo
+        >>> print_indexes(["Toyota", "Jeep", "Volvo"])
+        0 Toyota
+        1 Jeep
+        2 Volvo
 
-#     """
-
-#     print "Nothing at all"
+    """
+    for i,items in enumerate(my_list):
+        print i,  items
 
 
 # def long_words(word_list):
