4bcddf4850cfa9b7db730d90ad92d0d89bf4976e
Megan Peterson <Meganerd@Megans-MacBook-Pro.local>
2016-01-17 15:51:04 -0800

    All the problems, but can't figure out how to dedupe the last problem...

--- skills_week2.py
+++ skills_week2.py
@@ -49,6 +49,7 @@ def find_unique_common_items(list1, list2):
         [1, 2]
 
     """
+    # SET MATH!
     new_set = set(list1) & set(list2)
     return new_set
 
@@ -196,37 +197,49 @@ def sort_by_word_length(words):
     return sorted_list
 
 
-# def get_sum_zero_pairs(input_list):
-#     """Given list of numbers, return list of pair summing to 0.
+def get_sum_zero_pairs(input_list):
+    """Given list of numbers, return list of pair summing to 0.
 
-#     Given a list of numbers, add up each individual pair of numbers.
-#     Return a list of each pair of numbers that adds up to 0.
+    Given a list of numbers, add up each individual pair of numbers.
+    Return a list of each pair of numbers that adds up to 0.
 
 
-#     For example:
+    For example:
 
-#         >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1]) )
-#         [[-2, 2], [-1, 1]]
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1]) )
+        [[-2, 2], [-1, 1]]
 
-#         >>> sort_pairs( get_sum_zero_pairs([3, -3, 2, 1, -2, -1]) )
-#         [[-3, 3], [-2, 2], [-1, 1]]
+        >>> sort_pairs( get_sum_zero_pairs([3, -3, 2, 1, -2, -1]) )
+        [[-3, 3], [-2, 2], [-1, 1]]
 
-#     This should always be a unique list, even if there are
-#     duplicates in the input list:
+    This should always be a unique list, even if there are
+    duplicates in the input list:
 
-#         >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1]) )
-#         [[-2, 2], [-1, 1]]
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1]) )
+        [[-2, 2], [-1, 1]]
 
-#     Of course, if there are one or more zeros to pair together,
-#     that's fine, too (even a single zero can pair with itself):
+    Of course, if there are one or more zeros to pair together,
+    that's fine, too (even a single zero can pair with itself):
 
-#         >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1, 0]) )
-#         [[-2, 2], [-1, 1], [0, 0]]
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1, 0]) )
+        [[-2, 2], [-1, 1], [0, 0]]
 
-#     """
+    """
 
-#     return []
+    # for num1 in range(0, len(input_list)):
+    #     for num2 in range(0, len(input_list)):
+    #         if num1 + num2 == 0:
+    #             final_pairs = [num1, num2]
+    #             break
+    pairs = []
+    check = set(pairs)
 
+    for num1 in input_list:
+        for num2 in input_list:
+            if num1 + num2 == 0:
+                pairs.append([num1, num2])
+
+    return pairs
 
 # ##############################################################################
 # # You can ignore everything below this.
